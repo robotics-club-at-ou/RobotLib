6a27f7eb47c2db5363fbf12ee10fac69be475267
noahzemlin <noah.zemlin@ou.edu>
2019-02-04 18:22:22 -0600

    Removed unneccesary classes for Robotics Club. Added standard library files.

---
+++ src/RobotLib.h
@@ -0,0 +1,11 @@
+#ifndef ROBOTLIB_H
+#define ROBOTLIB_H
+
+/**
+ * The purpose of this header file is to be able to include the whole library 
+ * with one include statement.
+ */
+
+#include "Motor.h"
+
+#endif
